0737061fb62a35cb30b29b955edc9661ba477f34
Daniele Rondina <geaaru@sabayonlinux.org>
2021-11-21 11:34:07 +0100

    Prepare release v0.4.0

--- cmd/root.go
+++ cmd/root.go
@@ -35,7 +35,7 @@ const (
 Tar-formers - A golang tool to control tar flows
 `
 
-	TARFORMERS_VERSION = `0.3.6`
+	TARFORMERS_VERSION = `0.4.0`
 )
 
 // Build time and commit information. This code is get from: https://github.com/mudler/luet/
